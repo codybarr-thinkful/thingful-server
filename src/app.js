8f31e899ee98ce39c3ab09d11fcf154c0b7a6924
Cody Barr <cody.barr@gmail.com>
2020-01-12 17:57:28 -0500

    jwt-checkpoint-3

--- src/app.js
+++ src/app.js
@@ -9,9 +9,11 @@ const reviewsRouter = require('./reviews/reviews-router')
 
 const app = express()
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test',
-}))
+app.use(
+	morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+		skip: () => NODE_ENV === 'test'
+	})
+)
 app.use(cors())
 app.use(helmet())
 
@@ -19,14 +21,14 @@ app.use('/api/things', thingsRouter)
 app.use('/api/reviews', reviewsRouter)
 
 app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: 'Server error' }
-  } else {
-    console.error(error)
-    response = { error: error.message, object: error }
-  }
-  res.status(500).json(response)
+	let response
+	if (NODE_ENV === 'production') {
+		response = { error: 'Server error' }
+	} else {
+		console.error(error)
+		response = { error: error.message, object: error }
+	}
+	res.status(500).json(response)
 })
 
 module.exports = app
